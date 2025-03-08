99434f24c5e52eb5ae8db1f859a953ac862ab489
Shubham jha <sjha870054@gmail.com>
2025-03-09 01:31:22 +0530

    09-03

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,19 @@ import Unitplans from "./components/Unitplans";
 import Testimonials from "./components/Testimonials";
 import NewsletterSubscription from "./components/Newsletter";
 import Footer from "./components/Footer";
+import SansaraEnquiryForm from "./components/SansaraEnquiry";
+import EnquireForm from "./EnquireForm";
 
 function App() {
   return (
     <div>
       <Overview />
+      <EnquireForm />
       <Higlights />
       <Siteplans />
       <Unitplans />
       <Testimonials />
+      <SansaraEnquiryForm />
       <NewsletterSubscription />
       <Footer />
     </div>
