42b72189f8a2f431f26a23c002b042f10d27c546
Shubham jha <sjha870054@gmail.com>
2025-03-21 09:21:54 +0530

    update

--- src/components/ThankYouModal.jsx
+++ src/components/ThankYouModal.jsx
@@ -12,7 +12,7 @@ const ThankYouModal = ({ onClose, name }) => {
   console.log("Opened");
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 px-3">
       <div className="bg-white rounded-lg p-8 max-w-md w-full relative">
         {/* X button to close the modal */}
         <button
